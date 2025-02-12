1a89fb9bf20034c2528dad921971c08675abe228
Viettrung <nguyenviettrung2005@gmail.com>
2025-02-12 22:51:42 +0700

    finish

--- Pokemon/index.js
+++ Pokemon/index.js
@@ -1,35 +1,70 @@
-const list = document.querySelector('.section-list')
-const button = document.querySelector('.button')
-let lastPos = 0
-const input = document.querySelector('.input')
+const list = document.querySelector('.section-list');
+const button = document.querySelector('.button');
+const input = document.querySelector('.search');
 
+let lastPosition = 36;
+let data = [];
+
+// Hàm hiển thị danh sách Pokémon
 function displayPokemons(pokemons) {
-    document.getElementById("pokemon-list").innerHTML = pokemons.map(p => `
-        <div class="pokemon">
-            <h2>${p.name}</h2>
-            <p>Type: ${p.type}</p>
-        </div>
+    list.innerHTML = pokemons.map(p => `
         <div class="list-item">
-                <a href="/bulbasaur" class="pokemon">
-                    <div class="id">#1</div>
-                        <div class="image" style="background-image: url(&quot;https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png&quot;);"></div>
-                        <h3 class="title">bulbasaur</h3>
-                        <div class="labels">
-                            <div class="label grass" data-v-996d4743="">grass</div>
-                            <div class="label poison" data-v-996d4743="">poison</div>
-                        </div>
-                    </a>
+            <a href="/${p.name}" class="pokemon">
+                <div class="id">#${p.id}</div>
+                <div class="image" style="background-image: url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.id}.png');"></div>
+                <h3 class="name">${p.name}</h3>
+                <div class="types">
+                    ${p.types.map(type => `<div class="type ${type}">${type}</div>`).join("")}
                 </div>
-            </div>
+            </a>
+        </div>
     `).join("");
 }
 
+// Hàm tìm kiếm Pokémon
 function searchPokemon() {
-    const searchValue = document.getElementById("search").value.toLowerCase();
-    const filtered = window.pokemons.filter(p => p.name.toLowerCase().includes(searchValue));
+    const searchValue = input.value.toLowerCase();
+    const filtered = data.filter(p => p.name.toLowerCase().includes(searchValue));
     displayPokemons(filtered);
 }
 
+// Gọi API lấy danh sách Pokémon
+async function fetchPokemons() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=1302");
+        const result = await response.json();
+        
+        // Lấy thông tin từng Pokémon (name, id, types)
+        data = await Promise.all(result.results.map(async (p, index) => {
+            const details = await fetch(p.url).then(res => res.json());
+            return {
+                id: details.id,
+                name: details.name,
+                types: details.types.map(t => t.type.name)
+            };
+        }));
+
+        window.pokemons = data; 
+        displayPokemons(data.slice(0, lastPosition)); 
+}
+
+fetchPokemons();
+
+input.addEventListener("input", function() {
+    const searchValue = input.value.toLowerCase();
+    if (searchValue === "") {
+        displayPokemons(data.slice(0, lastPosition));
+        return;
+    }
+    const filtered = data.filter(p => p.name.toLowerCase().includes(searchValue));
+    displayPokemons(filtered);
+    button.style.display = 'none';
+});
+
 button.addEventListener('click', function() {
-    handleLoadMore()
-})
+    lastPosition += 36;
+    if (lastPosition > data.length) {
+        lastPosition = data.length;
+        button.style.display = 'none';
+    }
+    displayPokemons(data.slice(0, lastPosition));
+});
